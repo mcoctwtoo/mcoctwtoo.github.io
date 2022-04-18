bba5a7d56aea31b0e57b9d1bce9721b5d4067334
mcoctwtoo <103780215+mcoctwtoo@users.noreply.github.com>
2022-04-18 17:48:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-var http = require("http");
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
- }).listen(8080);
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
